fcaf29364f1a0ffac2c0c8f604b0262cc7a7d553
Ri-yan <mohdriyan08011995@gmail.com>
2022-04-21 20:24:55 +0530

    f

--- cypress/integration/MyTests/test.js
+++ cypress/integration/MyTests/test.js
@@ -6,7 +6,7 @@ describe('Test',()=>{
     //     cy.visit('/login')
     // })
     it('Test for checking heading',()=>{
-    cy.visit('https://authentication-only.netlify.app/login')
+    cy.visit('https://authentication-only.netlify.app')
     cy.get('.card-body').contains('Log In').should('have.text','Log In') 
     })
 
