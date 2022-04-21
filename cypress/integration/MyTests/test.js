0706e6bbe3c04dd43a4f6b5093bce9263d4e3053
Ri-yan <mohdriyan08011995@gmail.com>
2022-04-21 20:15:36 +0530

    q

--- cypress/integration/MyTests/test.js
+++ cypress/integration/MyTests/test.js
@@ -5,8 +5,8 @@ describe('Test',()=>{
     //  beforeEach(()=>{
     //     cy.visit('/login')
     // })
-    cy.visit('/login')
     it('Test for checking heading',()=>{
+    cy.visit('/login')
     cy.get('.card-body').contains('Log In').should('have.text','Log In') 
     })
 
